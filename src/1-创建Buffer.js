ddcefeb03294662cbbdc73ce3a37e0db655cfc70
ablikim915 <1057908611@qq.com>
2023-05-23 21:52:51 +0800

    learn: day2

--- src/1-创建Buffer.js
+++ src/1-创建Buffer.js
@@ -7,7 +7,7 @@ let buf2 = Buffer.allocUnsafe(10000)
 // console.log(buf2)
 
 // 3. from
-let buf3 = Buffer.from('hello')
-let buf4 = Buffer.from([1,2,3])
-console.log(buf3); // <Buffer 68 65 6c 6c 6f>
+let buf3 = Buffer.from('i love you')
+let buf4 = Buffer.from([105, 32, 108, 111, 118, 101, 32, 121, 111, 117])
+console.log(buf3); // <Buffer 68 65 6c 6c 6f> 先从字符集合中把每个字符对应的Unicode查出来，然后Unicode转换成16进制存到buffer中。
 console.log(buf4); // <Buffer 01 02 03>
