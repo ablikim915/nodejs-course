e15eddeedd8b08d150f0bbff522346fadbc2b0cc
ablikim915 <1057908611@qq.com>
2023-07-09 21:43:35 +0800

    chore: 遗漏内容提交

---
+++ src/1-Buffer模块/2-操作Buffer.js
@@ -0,0 +1,19 @@
+// buffer与字符串转换
+let buf1 = Buffer.from([105, 32, 108, 111, 118, 101, 32, 121, 111, 117])
+// console.log(buf1.toString())
+
+//
+let buf2 = Buffer.from('hello');
+console.log(buf2[4].toString(10)) // 转10进制 111
+buf2[1] = 111;
+console.log(buf2.toString())  // hollo
+
+// 溢出
+let buf3 = Buffer.from('hello');
+buf3[0] = 361; // 舍弃高于8位的数字，因为utf-8解码中1字节等于8位 361 => 0001 0110 1001 => 0110 1001
+console.log(buf3) // <Buffer 69 65 6c 6c 6f>
+console.log(buf3.toString()) // iello
+
+// 中文 
+let buf4 = Buffer.from('你好')
+console.log(buf4); // <Buffer e4 bd a0 e5 a5 bd>  utf-8的中文字符，一个中文字占3个字节
